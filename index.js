9d34b9cc001a9497204e80d54f3811b3af443ee9
soclarence <93038777+soclarence@users.noreply.github.com>
2022-01-03 21:05:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: 1,
+});
